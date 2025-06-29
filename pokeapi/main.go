667b8d74d86fbfce1a64a36c9fbae84e93a5217e
Joseph Vranicar <josephvranicar1@gmail.com>
2025-06-29 15:38:49 -0400

    c

--- pokeapi/main.go
+++ pokeapi/main.go
@@ -13,40 +13,44 @@ func parseTemp(f string) *template.Template {
 	return template.Must(template.ParseFiles(f))
 }
 
-func main() {
+func mainPageHandle(w http.ResponseWriter, r *http.Request) {
 	dex, dexErr := pokeapi.Pokedex("national")
 	if (dexErr != nil) {
 		log.Fatalln(dexErr)
 	}
 
+	template.Must(template.ParseFiles("main.html")).Execute(w, dex.PokemonEntries)
+}
+
+func pkmnLoadfunc(w http.ResponseWriter, r *http.Request) {
+	pkmnNum := r.PathValue("num")
+
+	type PkmnData struct {
+		Pokemon structs.Pokemon
+		PokemonSpecies structs.PokemonSpecies
+	}
+	
+	p, pErr := pokeapi.Pokemon(pkmnNum)
+	if (pErr != nil) {
+		log.Fatalln(pErr)
+	}
+
+	s, sErr := pokeapi.PokemonSpecies(pkmnNum)
+	if (sErr != nil) {
+		log.Fatalln(sErr)
+	}
+
+	data := PkmnData{Pokemon: p, PokemonSpecies: s}
+
+	parseTemp("pkmn.html").Execute(w, data)
+}
+
+func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		template.Must(template.ParseFiles("main.html")).Execute(w, dex.PokemonEntries)
-	})
+	http.HandleFunc("/home", mainPageHandle)
+	http.HandleFunc("/pkmn/{num}", pkmnLoadfunc)
 
-	http.HandleFunc("/pkmn/{num}", func(w http.ResponseWriter, r *http.Request) {
-		pkmnNum := r.PathValue("num")
-
-		type PkmnData struct {
-			Pokemon structs.Pokemon
-			PokemonSpecies structs.PokemonSpecies
-		}
-		
-		p, pErr := pokeapi.Pokemon(pkmnNum)
-		if (pErr != nil) {
-			log.Fatalln(pErr)
-		}
-
-		s, sErr := pokeapi.PokemonSpecies(pkmnNum)
-		if (sErr != nil) {
-			log.Fatalln(sErr)
-		}
-
-		data := PkmnData{Pokemon: p, PokemonSpecies: s}
-
-		parseTemp("pkmn.html").Execute(w, data)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe("localhost:8080", nil)
 }
+
