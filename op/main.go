7c61b9fc693d5ebae7738dbbf35cbfb4b4067126
Joseph Vranicar <josephvranicar1@gmail.com>
2025-07-05 20:18:42 -0400

    c

--- op/main.go
+++ op/main.go
@@ -7,53 +7,17 @@ func main() {
 		end int
 	}
 	
-var eastBlue ArcInfo
-var alabasta ArcInfo
-var skyIsland ArcInfo
-var water7 ArcInfo
-var thrillerBark ArcInfo
-var summitWar ArcInfo
-var fishmanIsland ArcInfo
-var dressrosa ArcInfo
-var wholeCakeIsland ArcInfo
-var wanoCountry ArcInfo
-var final ArcInfo
-
-eastBlue.name = "East Blue"
-alabasta.name = "Alabasta"
-skyIsland.name = "Sky Island"
-water7.name = "Water 7"
-thrillerBark.name = "Thriller Bark"
-summitWar.name = "Summit War"
-fishmanIsland.name = "Fishman Island"
-dressrosa.name = "Dressrosa"
-wholeCakeIsland.name = "Whole Cake Island"
-wanoCountry.name = "Wano Country"
-final.name = "Final Saga"
-
-eastBlue.start = 1
-alabasta.start = 62
-skyIsland.start = 144
-water7.start = 303
-thrillerBark.start = 337
-summitWar.start = 385
-fishmanIsland.start = 517
-dressrosa.start = 579
-wholeCakeIsland.start = 751
-wanoCountry.start = 890
-final.start = 1086
-
-eastBlue.end = 61
-alabasta.end = 143
-skyIsland.end = 206
-water7.end = 336
-thrillerBark.end = 384
-summitWar.end = 516
-fishmanIsland.end = 578
-dressrosa.end = 750
-wholeCakeIsland.end = 889
-wanoCountry.end = 1085
-final.end = 2000
+eastBlue := ArcInfo{name: "East Blue", start: 1, end: 61}
+alabasta := ArcInfo{name: "Alabasta", start: 62, end: 143}
+skyIsland := ArcInfo{name: "Sky Island", start: 144, end: 206}
+water7 := ArcInfo{name: "Water 7", start: 303, end: 336}
+thrillerBark := ArcInfo{name: "Thriller Bark", start: 337, end: 384}
+summitWar := ArcInfo{name: "Summit War", start: 385, end: 516}
+fishmanIsland := ArcInfo{name: "Fishman Island", start: 517, end: 578}
+dressrosa := ArcInfo{name: "Dressrosa", start: 579, end: 750}
+wholeCakeIsland := ArcInfo{name: "Whole Cake Island", start: 751, end: 889}
+wanoCountry := ArcInfo{name: "Wano Country", start: 890, end: 1085}
+final := ArcInfo{name: "Final Saga", start: 1086, end: 2000}
 
 
 }
--- pokeapi/go.mod
+++
@@ -1,5 +0,0 @@
-module example/jv-pokeapi
-
-go 1.24.4
-
-require github.com/mtslzr/pokeapi-go v1.4.0
